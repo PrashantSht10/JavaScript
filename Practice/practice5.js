c4cca79843dcbb1da01c408bd0ba4dd23e6be25c
Prashant Shrestha <prashantsht700@gmail.com>
2024-06-21 20:52:24 +0545

    arrays

---
+++ Practice/practice5.js
@@ -0,0 +1,49 @@
+// Arrays 
+
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+
+console.log(newYearsResolutions);
+
+// Create an Array
+
+const hobbies = ['Football', 'Movies', 'Programming'];
+
+console.log(hobbies);
+
+// Accessing Elements
+
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+let listItem = famousSayings[0];
+
+console.log(listItem);
+
+console.log(famousSayings[2]);
+
+console.log(famousSayings[3]);
+
+// Update Elements
+
+let groceryList = ['bread', 'tomatoes', 'milk'];
+
+groceryList[1] = 'avocados';
+
+console.log(groceryList);
+
+// Arrays with let and const
+
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+
+console.log(condiments);
+
+condiments = ['Mayo'];
+
+console.log(condiments);
+
+utensils[3] = 'Spoon';
+
+console.log(utensils);
