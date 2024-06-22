c29d31c3943835567af0bdc160b6bab5a1f8ade6
Prashant Shrestha <prashantsht700@gmail.com>
2024-06-22 13:26:49 +0545

    Arrays

--- Practice/practice5.js
+++ Practice/practice5.js
@@ -47,3 +47,83 @@ console.log(condiments);
 utensils[3] = 'Spoon';
 
 console.log(utensils);
+
+// The .length property
+
+const objectives = ['Learn a new language', 'Read 52 books', 'Run a marathon'];
+
+console.log(objectives.length);
+
+// The .push() Method
+
+let chores = ['wash dishes', 'do laundry', 'take out trash'];
+
+chores.push('do assignment', 'do laundry');
+
+console.log(chores);
+
+// The .pop() Method
+
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+
+let removed = chores.pop();
+
+console.log(chores);
+
+console.log(removed);
+
+// More Array Methods
+
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.shift();
+
+console.log(groceryList);
+
+groceryList.unshift('popcorn');
+
+console.log(groceryList);
+
+console.log(groceryList.slice(1, 4));
+
+console.log(groceryList);
+
+const pastaIndex = groceryList.indexOf('pasta');
+
+console.log(pastaIndex);
+
+// Arrays and Functions
+
+const concept = ['arrays', 'can', 'be', 'mutated'];
+
+function changeArr(arr){
+  arr[3] = 'MUTATED';
+}
+
+changeArr(concept);
+
+console.log(concept);
+
+function removeElement (newArr) {
+  newArr.pop();
+}
+
+removeElement(concept);
+
+console.log(concept);
+
+// Nested Arrays
+
+let numberClusters = [[1, 2], [3, 4], [5, 6]];
+
+const target = numberClusters[2][1];
+
+console.log(target);
+
+// Uses the .length property to find the last element of an array
+
+const colors = ['red', 'green', 'blue', 'black'];
+
+const arr = colors[colors.length - 1];
+
+console.log(arr);
