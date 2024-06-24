302923d7eb1d45a2f24fe4fae77ba7bcf251074a
Prashant Shrestha <prashantsht700@gmail.com>
2024-06-24 21:00:33 +0545

    Loops

---
+++ Practice/practice6.js
@@ -0,0 +1,9 @@
+// Loops
+
+// Repeating Tasks Manually
+
+let vacationSpots = ['Paris', 'New York', 'Liverpool'];
+
+console.log(vacationSpots[0]); // prints Paris
+console.log(vacationSpots[1]); // prints New York
+console.log(vacationSpots[2]); // prints Liverpool
